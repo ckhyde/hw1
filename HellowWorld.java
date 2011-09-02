b03e46665a58d7285129c843639bbcec01312aa3
Chun-Yi Yang <ckhyde.yang@gmail.com>
2011-09-02 00:39:32 -0400

    V 1.0

---
+++ HellowWorld.java
@@ -0,0 +1,27 @@
+//: hw1/HellowWorld.java
+
+/*
+ * The first exercise for CSS 605.
+ */
+package edu.gmu.css.css605fall2011.hw1;
+
+/**
+ *
+ * @author Chun-Yi Yang
+ * @author G00567451
+ * @version 1.0
+ * @since 09/02/2011
+ * 
+ */
+public class HellowWorld {
+
+    /**
+     * @param args the command line arguments
+     * @return null
+     */
+    public static void main(String[] args) {
+         
+        // print out "Hellow, World" on the console
+        System.out.println("Hellow, World");
+    }
+}
